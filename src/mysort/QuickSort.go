8f21c9ed743b9f7f0b567c248c0cdcf19b62dc1e
jianying.wcj <jianying.wcj@alibaba-inc.com>
2017-09-19 23:30:43 +0800

    添加quicksort

--- src/mysort/QuickSort.go
+++ src/mysort/QuickSort.go
@@ -1,27 +1,37 @@
 package mysort
-
+import "fmt"
 // BubbleSort
-// 时间复杂度 O(n)* O(n) = O(n^2)
-// 空间复杂度 O(n)
+// 时间复杂度 O(n)* O(lgn) = O(nlgn)
 //外层循环只是用来控制次数
 //内层循环保证比较和交换
 func QuickSort(input [] int,left int,right int) []int {
-	low,high := left,right
-	key := input[low]
-	for left < right {
-		for input[high] > key && low < high {
-			high --
-		}
-		input[low] = input[high]
+	if left < right {
+		low,high := left,right
+		key := input[low]
+		for low < high {
+			for input[high] > key && low < high {
+				high --
+			}
+			input[low] = input[high]
 
-		for input[low] < key && low < high {
-			low ++
+			for input[low] < key && low < high {
+				low ++
+			}
+			input[high] = input[low]
 		}
-		input[high] = input[low]
+		input[low] = key
+		fmt.Println("====result====")
+		fmt.Println(input)
+		fmt.Println("========")
+		fmt.Println("====left sub====")
+		fmt.Println(input[0:low])
+		fmt.Println("========")
+		fmt.Println("====right sub====")
+		fmt.Println(input[low+1:right+1])
+		fmt.Println("========")
+		QuickSort(input,left,low-1)
+		QuickSort(input,low+1,right)
 	}
-	input[low] = key
-	QuickSort(input[0:low],left,low-1)
-	QuickSort(input[low+1:right],low+1,right)
 
 	return input
 
