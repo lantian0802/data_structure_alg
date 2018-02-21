f2e28e8a1726d3c5fa95505b74860b6b194b2b5a
jianying.wcj <jianying.wcj@alibaba-inc.com>
2018-02-21 21:27:34 +0800

    add

--- src/main/Driver.go
+++ src/main/Driver.go
@@ -10,5 +10,5 @@ import (
 func main() {
 	var numList = []int{1, 32, 2, 7, 6, 4}
 	//0 1 1 1 1 0 1 1
-	fmt.Println(mysort.BucketSort(numList))
+	fmt.Println(mysort.HeapSort(numList))
 }
---
+++ src/mysort/HeapSort.go
@@ -0,0 +1,34 @@
+package mysort
+
+func HeapSort(numList []int) []int {
+	for i := len(numList)/2 - 1; i >= 0; i-- {
+		AdjustHeap(numList, i, len(numList)-1)
+	}
+
+	for j := len(numList) - 1; j >= 0; j-- {
+		temp := numList[0]
+		numList[0] = numList[j]
+		numList[j] = temp
+		AdjustHeap(numList, 0, j-1)
+	}
+	return numList
+}
+
+func AdjustHeap(numList []int, head int, length int) {
+	temp := numList[head]
+	index := 0
+
+	for index = 2 * head; index < length; index *= 2 {
+		if index < length && numList[index] < numList[index+1] {
+			index++
+		}
+
+		if temp >= numList[index] {
+			break
+		}
+
+		numList[head] = numList[index]
+		head = index
+	}
+	numList[head] = temp
+}
